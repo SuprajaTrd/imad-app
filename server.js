46ccc595f3c0b6bef103aedfd819a0b387252ae9
SuprajaTrd <suprajadinakaran@gmail.com>
2017-08-15 20:14:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article1',function(req,res){
-   res.send("Article1"); 
+   res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
